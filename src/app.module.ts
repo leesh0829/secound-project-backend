e03726dcc20ac269514fa400309e27f875e3ea6d
Adelie <lsh77392125@gmail.com>
2024-08-14 17:08:19 +0900

    20240814

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
-  }),
+  }), PrismaModule,
 ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { PrismaService } from './prisma/prisma.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
   await app.listen(3000);
+  await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
